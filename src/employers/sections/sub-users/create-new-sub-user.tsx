b9fcc5108bb1d0f16dfb24e0f197af2f473732f4
aabi0044 <Mabdulah596@hotmail.com>
2020-03-17 02:51:16 +0500

    update

--- src/employers/sections/sub-users/create-new-sub-user.tsx
+++ src/employers/sections/sub-users/create-new-sub-user.tsx
@@ -8,15 +8,99 @@ type CreateNewSubUserProps = {
 const CreateNewSubUser: FunctionComponent<CreateNewSubUserProps> = ({ children }) => {
 
     return (
-        
-        <div className=" container p-0 check" style={{paddingLeft:"0px"}}>
+
+        <div className=" container p-0 check" style={{ paddingLeft: "0px" }}>
             {children}
             <Form className="job-form">
-        <Row>
-            <Col>
-            <span> Fields marked with asterisk (*) are mandatory</span>
-            </Col>
-        </Row>
+                <Row>
+                    <Col>
+                        <span> Fields marked with asterisk (*) are mandatory</span>
+                    </Col>
+                </Row>
+
+                <Row className="mt-3">
+                    <Col md="6" sm="12">
+                        <FormGroup>
+                            <Label for="exampleEmail" className="text-left">Username <b style={{ color: 'red' }}>*</b> </Label>
+                            <Input className="form-input" type="text" name="jobTitle" id="job-title" placeholder="Enter  username" />
+                        </FormGroup>
+
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md="6" sm="12">
+                        <FormGroup>
+                            <Label for="exampleEmail" className="text-left">Password <b style={{ color: 'red' }}>*</b> </Label>
+                            <Input className="form-input" type="password" name="jobTitle" id="job-title" placeholder="Enter  password" />
+                        </FormGroup>
+
+                    </Col>
+                    <Col md="6" sm="12">
+                        <FormGroup>
+                            <Label for="exampleEmail" className="text-left">Confirm Password <b style={{ color: 'red' }}>*</b> </Label>
+                            <Input className="form-input" type="password" name="jobTitle" id="job-title" placeholder="Enter password again" />
+                        </FormGroup>
+
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="6" sm="12">
+                        <FormGroup>
+                            <Label for="exampleEmail" className="text-left">Email <b style={{ color: 'red' }}>*</b> </Label>
+                            <Input className="form-input" type="email" name="jobTitle" id="job-title" placeholder="Enter email" />
+                        </FormGroup>
+
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="6" sm="12">
+                        <FormGroup className="mb-1">
+                            <Label for="exampleEmail" className="text-left">UserRights <b style={{ color: 'red' }}>*</b> </Label>
+
+                        </FormGroup>
+
+                    </Col>
+                </Row>
+                <Row className="mt-1">
+                    <Col>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" className="checkbox" />{' '}
+                                     Post Jobs
+                                        </Label>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row className="mt-1">
+                    <Col>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" className="checkbox" />{' '}
+                                    Manage listings and applications
+                                        </Label>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row className="mt-1">
+                    <Col>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" className="checkbox" />{' '}
+                                 Manage Templates
+                                        </Label>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row className="mt-5 mb-3">
+                    <Col md="6" sm="12" className="text-left">
+                        <span className="text-danger">erorr:xxxxxxxxxxxxxxxxx</span>
+                    </Col>
+                    <Col md={{ size: 6 }} sm='12' className="text-right">
+                        <Button className="button-no-bg">CANCEL</Button>
+                        <Button className="button-gradient">SAVE & PREVIEW</Button>
+                    </Col>
+                </Row>
             </Form>
 
         </div>
--- src/employers/sections/sub-users/existing-sub-users.tsx
+++ src/employers/sections/sub-users/existing-sub-users.tsx
@@ -1,99 +1,109 @@
 import React, { FunctionComponent, useState } from "react";
 import { Container, Table, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import '../../employers.styles.css'
+import EditSubUser from "./edit-sub-user";
+import SubUserDetails from "./sub-user-details";
 
-type ExistingSubUsersProps={
+type ExistingSubUsersProps = {
 
 }
-const ExistingSubUsers:FunctionComponent<ExistingSubUsersProps> = ({children}) =>{
+const ExistingSubUsers: FunctionComponent<ExistingSubUsersProps> = ({ children }) => {
     const [modal, setModal] = useState(false);
     const [deleteModal, setDeleteModal] = useState(false);
+    const [detailModal, setDetailModal] = useState(false);
     const [existingSubUsers] = useState([
-      {
-        id: 'hsdkfhsdlkfh',
-        username: "XXXXXXXXXXXXXXXXXXXXXXX",
-        email:"xx@gmail.com"
-      },
-      {
-        id: 'hsdkfhsdlkfh',
-        username: "XXXXXXXXXXXXXXXXXXXXXXX",
-        email:"xx@gmail.com"
-      },
-      {
-        id: 'hsdkfhsdlkfh',
-        username: "XXXXXXXXXXXXXXXXXXXXXXX",
-        email:"xx@gmail.com"
-      },
-      {
-        id: 'hsdkfhsdlkfh',
-        username: "XXXXXXXXXXXXXXXXXXXXXXX",
-        email:"xx@gmail.com"
-      },
+        {
+            id: 'hsdkfhsdlkfh',
+            username: "XXXXXXXXXXXXXXXXXXXXXXX",
+            email: "xx@gmail.com"
+        },
+        {
+            id: 'hsdkfhsdlkfh',
+            username: "XXXXXXXXXXXXXXXXXXXXXXX",
+            email: "xx@gmail.com"
+        },
+        {
+            id: 'hsdkfhsdlkfh',
+            username: "XXXXXXXXXXXXXXXXXXXXXXX",
+            email: "xx@gmail.com"
+        },
+        {
+            id: 'hsdkfhsdlkfh',
+            username: "XXXXXXXXXXXXXXXXXXXXXXX",
+            email: "xx@gmail.com"
+        },
     ]);
     const toggle = () => setModal(!modal);
 
     const editClickHandler = (id: string) => {
-      setModal(true);
+        setModal(true);
     }
-    return(
-       <div>
-           {children}
-           <Row className="">
-        <Col md="12" xs="12" sm="12">
-          <Table hover className="mt-5 active-job-unprocessed-table">
-            <thead className="active-job-unprocessed-table-thead bg-primary text-white mt-3 py-2">
-              <tr>
-                <th className="">User Name</th>
-                <th className="">Email</th>
-                <th className="">Actions</th>
-                
-              </tr>
-            </thead>
-            <tbody>
-              {existingSubUsers.map((user, i) => {
-                return (
-                  <tr key={i}>
-                    <td className="">{user.username}</td>
-                    <td className="">{user.email}</td>
-                    <td className="">
-                      <Button onClick={() => editClickHandler(user.id)} color="link">Edit</Button>
-                      <Button onClick={() => setDeleteModal(true)} color="link">Delete</Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-      <Row >
-        <Modal className="modal-lg custom-modal" style={{ width: 'width: 90vw' }} isOpen={modal} toggle={toggle} centered >
-          {/* <EditTemplateModal template={null} /> */}
-        </Modal>
-      </Row>
-      <Row>
-        <Modal isOpen={deleteModal} toggle={() => setDeleteModal(!deleteModal)} centered>
-          <Row className="p-4">
-            <Col>
+    const detailViewHandler = (id: string) => {
+        setDetailModal(true)
+    }
 
+    return (
+        <div>
+            {children}
+            <Row className="">
+                <Col md="12" xs="12" sm="12">
+                    <Table hover className="mt-5 active-job-unprocessed-table">
+                        <thead className="active-job-unprocessed-table-thead bg-primary text-white mt-3 py-2">
+                            <tr>
+                                <th className="">User Name</th>
+                                <th className="">Email</th>
+                                <th className="">Actions</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {existingSubUsers.map((user, i) => {
+                                return (
+                                    <tr key={i} onClick={() => detailViewHandler(user.id)}>
+                                        <td className="">{user.username}</td>
+                                        <td className="">{user.email}</td>
+                                        <td className="">
+                                            <Button onClick={() => editClickHandler(user.id)} color="link">Edit</Button>
+                                            <Button onClick={() => setDeleteModal(true)} color="link">Delete</Button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
             <Row >
-            <Col>
-              <h5 style={{color:'#375e65',fontWeight:'bold'}}> Confirm delete?</h5>
-            </Col>
-          </Row>
-          <Row className="mt-2 mb-3">
-            <Col md={{ size: 12 }} sm='12' className="text-right">
-              <Button className="button-no-bg" >CANCEL</Button>
-              <Button className="button-gradient modal-save-button" type="button">Yes </Button>
-            </Col>
-          </Row>
-            </Col>
-          </Row>
-     
-        </Modal>
-      </Row>
+                <Modal className="modal-lg custom-modal" style={{ width: 'width: 90vw' }} isOpen={modal} toggle={toggle} centered >
+                    <EditSubUser />
+                </Modal>
+                <Modal className="modal-lg custom-modal" style={{ width: 'width: 90vw' }} isOpen={detailModal} toggle={()=>setDetailModal(!detailModal)} centered >
+                    <SubUserDetails />
+                </Modal>
+            </Row>
+            <Row>
+                <Modal isOpen={deleteModal} toggle={() => setDeleteModal(!deleteModal)} centered>
+                    <Row className="p-4">
+                        <Col>
 
-       </div>
+                            <Row >
+                                <Col>
+                                    <h5 style={{ color: '#375e65', fontWeight: 'bold' }}> Confirm delete?</h5>
+                                </Col>
+                            </Row>
+                            <Row className="mt-2 mb-3">
+                                <Col md={{ size: 12 }} sm='12' className="text-right">
+                                    <Button className="button-no-bg" >CANCEL</Button>
+                                    <Button className="button-gradient modal-save-button" type="button">Yes </Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
+                </Modal>
+            </Row>
+
+        </div>
     )
 }
 export default ExistingSubUsers;
