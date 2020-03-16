e2e8199485471e63ef48cfa2c965cbe188340954
aabi0044 <Mabdulah596@hotmail.com>
2020-03-17 02:04:51 +0500

    sub users updated

--- src/employers/sections/sub-users/create-new-sub-user.tsx
+++ src/employers/sections/sub-users/create-new-sub-user.tsx
@@ -2,17 +2,24 @@ import React, { FunctionComponent } from 'react';
 import { Form, Button, FormGroup, FormText, Container, Row, Col, InputGroup, InputGroupText, InputGroupAddon, Input, Label } from 'reactstrap';
 import '../../employers.styles.css'
 
-type CreateNewSubUserProps={
+type CreateNewSubUserProps = {
 
 }
-const CreateNewSubUser:FunctionComponent<CreateNewSubUserProps> = ({children}) =>{
+const CreateNewSubUser: FunctionComponent<CreateNewSubUserProps> = ({ children }) => {
 
     return (
-    <Container>
-        {children}
-
         
-    </Container>
+        <div className=" container p-0 check" style={{paddingLeft:"0px"}}>
+            {children}
+            <Form className="job-form">
+        <Row>
+            <Col>
+            <span> Fields marked with asterisk (*) are mandatory</span>
+            </Col>
+        </Row>
+            </Form>
+
+        </div>
     )
 }
 export default CreateNewSubUser;
